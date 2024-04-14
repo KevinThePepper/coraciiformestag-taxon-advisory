1a5642ae77f839914d5d8c2d21dc5b255a12d813
Kevin Shelley <kevin.shelley@pm.me>
2024-04-14 09:23:53 -0700

    fix: emerald green

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
-const { theme } = require('@sanity/demo/tailwind')
+const { theme } = require("@sanity/demo/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './intro-template/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./intro-template/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     ...theme,
+    colors: {
+      ...theme.colors,
+      green: "#50c878"
+    },
     // Overriding fontFamily to use @next/font loaded families
     fontFamily: {
-      mono: 'var(--font-mono)',
-      sans: 'var(--font-sans)',
-      serif: 'var(--font-serif)',
+      mono: "var(--font-mono)",
+      sans: "var(--font-sans)",
+      serif: "var(--font-serif)",
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
